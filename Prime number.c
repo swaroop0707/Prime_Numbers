575dc307fc536132ecfcb8c3deb660b1f0e5951e
Satyabrata2029 <65301072+Satyabrata2029@users.noreply.github.com>
2020-05-16 20:06:49 +0530

    Written by Satyabrata Pradhan
    This code checks whether a number is prime or not

---
+++ Prime number.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int prime(int , int);
+int main()
+{
+	int n, i, isprime;
+	printf("Enter the number: ");
+	scanf("%d", &n);
+	i=n/2;
+	isprime = prime(n,i);
+	if(isprime==1)
+	{
+		printf("%d is a prime number", n);
+	}
+	else
+	{
+		printf("%d is not a prime number", n);
+	}
+	return 0;
+}
+int prime(int n, int i)
+{
+	if(i==1)
+	{
+		return 1;
+	}
+	else if(n%i==0)
+	{
+		return 0;
+	}
+	else
+	{
+		return prime(n, i-1);
+	}
+}
